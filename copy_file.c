4191ead14ac3c3eb378c8e0152b69ff7647bb4e4
99003604 <78540305+99003604@users.noreply.github.com>
2021-03-03 15:28:35 +0530

    Update copy_file.c

--- copy_file.c
+++ copy_file.c
@@ -4,23 +4,23 @@
 int main() 
 { 
     FILE *fptr1, *fptr2; 
-    char filename[100], d; 
+    char Filename[1000], d; 
   
-    printf("Enter the filename to open for reading \n"); 
-    scanf("%s", filename); 
-    fptr1 = fopen(filename, "read"); 
+    printf("Enter the Filename to open for reading \n"); 
+    scanf("%s", Filename); 
+    fptr1 = fopen(Filename, "read"); 
     if (fptr1 == NULL) 
     { 
-        printf("Cannot open file %s \n", filename); 
+        printf("Cannot open file %s \n", Filename); 
         exit(0); 
     } 
   
-    printf("Enter the filename to open for writing \n"); 
-    scanf("%s", filename); 
-    fptr2 = fopen(filename, "write"); 
+    printf("Enter the Filename to open for writing \n"); 
+    scanf("%s", Filename); 
+    fptr2 = fopen(Filename, "write"); 
     if (fptr2 == NULL) 
     { 
-        printf("Cannot open file %s \n", filename); 
+        printf("Cannot open file %s \n", Filename); 
         exit(0); 
     } 
     d = fgetc(fptr1); 
@@ -30,7 +30,7 @@ int main()
         d= fgetc(fptr1); 
     } 
   
-    printf("\nContents copied to %s", filename); 
+    printf("\nContents copied to %s", Filename); 
   
     fclose(fptr1); 
     fclose(fptr2); 
